03e151d80f2a6bad353896d6fa6b5f50860e3247
melvinkukwansafari <melvin.kukwan@student.moringaschool.com>
2023-02-10 08:46:44 +0300

    complete assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
+
+    self.end_with?(".")
     
   end
 
   def question?
 
+    self.end_with?("?")
+
   end
 
   def exclamation?
+
+    
+    self.end_with?("!")    
 
   end
 
   def count_sentences
 
+    sentences = self.split(/[.?!]/)
+    sentences.count { |sentence| sentence.strip.length > 0 }
   end
+
 end
